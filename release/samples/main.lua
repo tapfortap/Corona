d2136667166bfac722e667370ba0fb0ef69cf3e0
Philip Laird <phil@tapfortap.com>
2013-05-07 09:49:44 -0700

    Add some comments. A few minor fixes.

--- release/samples/main.lua
+++ release/samples/main.lua
@@ -4,42 +4,50 @@ local widget = require "widget"
 local MALE = 0
 local FEMALE = 0
 
+-- The ad view listener function. Prints the event's name and message to the console.
 local function adViewListener(event)
     print(event.name, "adViewListener " .. event.event .. " " .. event.message)
 end
 
+-- The app wall listener function. Prints the event's name and message to the console.
 local function appWallListener(event)
     print(event.name, "appWallListener " .. event.event .. " " .. event.message)
 end
 
+-- The interstitial listener function. Prints the event's name and message to the console.
 local function interstitialListener(event)
     print(event.name, "interstitialListener " .. event.event .. " " .. event.message)
 end
 
+-- Function to handle when the showAd button is clicked. It will create an ad view at the bottom center of the screen.
 local function showAd( event )
     if event.phase == "ended" then
-        tapfortap.createAdView(0,0)
+        tapfortap.createAdView(2,3)
     end
 end
 
+-- Function to handle when the removeAdView button is clicked. It will remove the current displayed ad view.
 local function removeAd( event )
     if event.phase == "ended" then
         tapfortap.removeAdView()
     end
 end
 
+-- Function to handle when the showInterstitial button is clicked. It will show an interstitial.
 local function showInterstitial( event )
     if event.phase == "ended" then
         tapfortap.showInterstitial()
     end
 end
 
+-- Funciton to handle when the showAppWall button is clicked. It will show an app wall.
 local function showAppWall( event )
     if event.phase == "ended" then
         tapfortap.showAppWall()
     end
 end
 
+-- Configure the showAd button.
 local showAdButton = widget.newButton{
     id = "showAdButton",
     label = "Show Banner",
@@ -49,6 +57,7 @@ local showAdButton = widget.newButton{
     onEvent = showAd
 }
 
+-- Configure the removeAdButton.
 local removeAdButton = widget.newButton{
     id = "removeAdButton",
     label = "Remove Banner",
@@ -58,6 +67,7 @@ local removeAdButton = widget.newButton{
     onEvent = removeAd
 }
 
+-- Configure the showInterstitial button.
 local showInterstitialButton = widget.newButton{
     id = "showInterstitialButton",
     label = "Show Interstitial",
@@ -67,6 +77,7 @@ local showInterstitialButton = widget.newButton{
     onEvent = showInterstitial
 }
 
+-- Configure the showAppWall button.
 local showAppWallButton = widget.newButton{
     id = "showAppWallButton",
     label = "Show AppWall",
@@ -76,22 +87,45 @@ local showAppWallButton = widget.newButton{
     onEvent = showAppWall
 }
 
+-- Setup some buttons to handle showing ads
 showAdButton.x = display.contentCenterX
 showAdButton.y = 100
+
 removeAdButton.x = display.contentCenterX
 removeAdButton.y = showAdButton.y + showAdButton.height + 15
+
 showInterstitialButton.x = display.contentCenterX
 showInterstitialButton.y = removeAdButton.y + removeAdButton.height + 15
+
 showAppWallButton.x = display.contentCenterX
 showAppWallButton.y = showInterstitialButton.y + showInterstitialButton.height + 15
 
-tapfortap.initialize("a49b3abbbefbb948776b0edaa8718367")
+-- Initialize Tap for Tap with your account API key
+tapfortap.initialize("YOUR API KEY")
+
+-- Set the user's year of birth
 tapfortap.setYearOfBirth(1989)
+
+-- Set the user's gender to male (0)
 tapfortap.setGender(MALE)
+
+-- Set the user's location
 tapfortap.setLocation(48.418264494187454, -123.30814361572266)
+
+-- Set your custom account id for the user
 tapfortap.setUserAccountId("test account")
+
+-- Prepare an interstitial so when showInterstitial() is called, it will load faster
 tapfortap.prepareInterstitial()
-tapfortap.prepareAppWal()
+
+-- Prepare an app wall so when showAppWall() is called, it will load faster
+tapfortap.prepareAppWall()
+
+-- Set the ad view listener
 tapfortap.setAdViewListener(adViewListener)
+
+-- Set the app wall listener
 tapfortap.setAppWallListener(appWallListener)
+
+-- Set the interstitial listener
 tapfortap.setInterstitialListener(interstitialListener)
